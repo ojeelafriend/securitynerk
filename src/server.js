808cb1f11ecffef81a3af860fa82edb0bc082569
odev-badwin <ts8irolon@gmail.com>
2021-09-20 00:32:45 -0300

    sign token

--- src/server.js
+++ src/server.js
@@ -1,9 +1,32 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
+const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
+
+const { masterKey } = require('./config/config');
 
 app.use(express.json(), express.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => {
+  res.send('Get /');
+});
+
+app.post('/authenticate', async (req, res) => {
+  const user = {
+    username: req.body.username,
+    password: req.body.password,
+  };
+  const payload = {
+    check: true,
+  };
+
+  if (user.username === 'root' && user.password === 'krnl123') {
+    const token = jwt.sign(payload, masterKey, { expiresIn: 1400 });
+    console.log('token: ' + token);
+  } else {
+    res.send('Bad request');
+  }
+});
+
 app.listen(3000, () => {
-  console.log("Connection successfully");
+  console.log('Connection successfully');
 });
